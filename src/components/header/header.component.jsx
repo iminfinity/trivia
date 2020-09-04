3dfdd04637f436f10573438603832744f1e25f5b
iminfinity <this.is.me.infinity@gmail.com>
2020-09-04 14:32:56 +0530

    addded header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-
+import "./header.styles.scss";
 const Header = ({ category, handleCategoryChange }) => {
   return (
-    <header>
+    <header className="header">
+      <h1>Trivia</h1>
       <select value={category} onChange={handleCategoryChange}>
         <option value="18">Computer Science</option>
         <option value="19">Mathematics</option>
